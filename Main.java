b7d76bab48d2dc4b9f28a551c5986a3c6bc096eb
Chronoswitha-K <milo.lee27@student.tamdistrict.org>
2024-09-26 10:00:57 -0700

    try to find the first string between angle brackets

--- Main.java
+++ Main.java
@@ -2,6 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scammer = new Scanner(System.in);
+
+        // MadLib stories
+        String madLib = "I went to the animal <noun>, the <plural_noun> and the <number> beasts were there.";
+        // madLib = "Once upon a time in a <adjective> land, a <noun> <adverb> grew.";
+        // madLib = "The <adjective> <noun> <verb>";
+
+        int firstLeftBracket = madLib.indexOf("<");
+        int firstRightBracket = madLib.indexOf(">");
+
+        String firstPrompt = madLib.substring(firstLeftBracket, firstLeftBracket);
+
+        System.out.println("Give me a " + firstPrompt);
     }
 }
