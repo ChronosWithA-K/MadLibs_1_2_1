08736d74f12386285dc19166919af610a51285f3
Daemons <143678373+ChronosWithA-K@users.noreply.github.com>
2024-10-01 17:00:21 +0000

    count how many times angle bracket appears in madLib

--- Main.java
+++ Main.java
@@ -2,25 +2,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scammer = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        final char charToCheck = '<';
 
         // MadLib stories
         String madLib = "I went to the animal <noun>, the <plural_noun> and the <number> beasts were there.";
         // madLib = "Once upon a time in a <adjective> land, a <noun> <adverb> grew.";
         // madLib = "The <adjective> <noun> <verb>";
 
+        int iterator = countChar(madLib, '<');
+        int i = 0;
+
+        while (i <= iterator) {
+            
+        }
+
         int firstLeftBracket = madLib.indexOf("<");
         int firstRightBracket = madLib.indexOf(">");
 
-        // String firstPrompt = madLib.substring((firstLeftBracket + 1), firstRightBracket);
+        String firstPrompt = madLib.substring((firstLeftBracket + 1), firstRightBracket);
 
-        // System.out.println("Give me a " + firstPrompt);
-        // String firstAnswer = scammer.nextLine();
+        System.out.println("Give me a " + firstPrompt);
+        String firstAnswer = sc.nextLine();
+    }
 
-        for (int i = 0; i < madLib.length(); i++) {
-            if (madLib.charAt(i) == ) {
+    public static int countChar(String str, char c) {
+        int charCount = 0;
 
-            }
-        }
+        for (int i=0; i < str.length(); i++) {
+            if(str.charAt(i) == c)
+            charCount++;
+        }       
+
+        return charCount;
     }
 }
