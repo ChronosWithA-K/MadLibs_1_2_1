8d7151be519d09c936bba0f1534c45022915e544
Daemons <143678373+ChronosWithA-K@users.noreply.github.com>
2024-10-03 02:19:58 +0000

    finish

--- Main.java
+++ Main.java
@@ -3,37 +3,47 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        final char charToCheck = '<';
 
-        // MadLib stories
-        String madLib = "I went to the animal <noun>, the <plural_noun> and the <number> beasts were there.";
-        // madLib = "Once upon a time in a <adjective> land, a <noun> <adverb> grew.";
-        // madLib = "The <adjective> <noun> <verb>";
+        // madlib stories
+        String madlib = "I went to the animal <noun>, the <plural noun> and the <number> beasts were there.";
+        // madlib = "Once upon a time in a <adjective> land, a <noun> <adverb> grew.";
+        // madlib = "The <adjective> <noun> <verb>.";
 
-        int iterator = countChar(madLib, '<');
-        int i = 0;
+        // Warn users because the third prompt in the first madlib doesn't pass legalityChecker
+        System.out.println("If you are prompted for a number, write it out as a word (e.g. five instead of 5)");
 
-        while (i <= iterator) {
-            
+        // While "less than" angle brackets still exist in the madlib string:
+        while (madlib.indexOf("<") != -1) {
+            String prompt = madlib.substring(madlib.indexOf("<") + 1, madlib.indexOf(">"));
+            String toReplace = madlib.substring(madlib.indexOf("<"), madlib.indexOf(">") + 1);
+
+            System.out.println("Please give me a(n) " + prompt + ".");
+            String answer = sc.nextLine();
+
+            // Check if the input is legal
+            while (true) {
+                if (!legalityChecker(answer)) {
+                    System.out.println("\nAnswers to the prompt have to contain only letters.");
+                    System.out.println("Please give me a(n) " + prompt + ".");
+                    answer = sc.nextLine();
+                } else {
+                    break;
+                }
+            }
+
+            madlib = madlib.replace(toReplace, answer);
         }
-
-        int firstLeftBracket = madLib.indexOf("<");
-        int firstRightBracket = madLib.indexOf(">");
-
-        String firstPrompt = madLib.substring((firstLeftBracket + 1), firstRightBracket);
-
-        System.out.println("Give me a " + firstPrompt);
-        String firstAnswer = sc.nextLine();
+        System.out.println(madlib);
     }
-
-    public static int countChar(String str, char c) {
-        int charCount = 0;
-
-        for (int i=0; i < str.length(); i++) {
-            if(str.charAt(i) == c)
-            charCount++;
-        }       
-
-        return charCount;
+    
+    // Checks if the string passed to it has only letters
+    public static boolean legalityChecker(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            // Checks if the character at the index is a letter; if not returns false
+            if ((Character.isLetter(str.charAt(i)) == false)) {
+               return false;
+            }
+        }
+        return true; // If all characters in the string are a letter, return true
     }
 }
